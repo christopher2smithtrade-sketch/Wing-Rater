65d36721095cb76ec71cddec340c740daf3f3363
christopher2smithtrade-sketch <christopher2smith.trade@gmail.com>
2026-06-19 17:16:37 -0400

    Wing Rater update 2026-06-19

--- wings_sw.js
+++ wings_sw.js
@@ -1,9 +1,26 @@
-const CACHE = 'wings-v1';
+const CACHE = 'wings-v2';
+const STATIC = ['./wings_rater.html', './chicken.mp3', './cluck.mp3'];
+
 self.addEventListener('install', e => {
-    e.waitUntil(caches.open(CACHE).then(c => c.addAll(['./wings_rater.html'])));
+    e.waitUntil(caches.open(CACHE).then(c => c.addAll(STATIC)));
+    self.skipWaiting();
 });
+
+self.addEventListener('activate', e => {
+    e.waitUntil(
+        caches.keys().then(keys =>
+            Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+        )
+    );
+    self.clients.claim();
+});
+
 self.addEventListener('fetch', e => {
     e.respondWith(
-        fetch(e.request).catch(() => caches.match(e.request))
+        fetch(e.request).then(res => {
+            const clone = res.clone();
+            caches.open(CACHE).then(c => c.put(e.request, clone));
+            return res;
+        }).catch(() => caches.match(e.request))
     );
 });
